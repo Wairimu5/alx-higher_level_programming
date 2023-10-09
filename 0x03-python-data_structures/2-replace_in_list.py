a4d88962d104ae48cd294b29d56f82d6a832e776
Wairimu5 <innonjue@gmail.com>
2023-10-09 12:18:15 -0700

    2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# a function that that replaces an element of a list at a specific position
+
+
+def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return None
+    length = len(my_list) - 1
+    while (length >= 0):
+        print("{:d}".format(my_list[length]))
+        length = length - 1
