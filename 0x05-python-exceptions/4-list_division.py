e8636fe38ac898ead35c4557cd7b01672f607f48
Wairimu5 <innonjue@gmail.com>
2023-10-25 11:01:59 -0700

    fixed

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
-    result = [0] * list_length  # Create a new list to store the division results
+    result = [0] * list_length
     for i in range(list_length):
         try:
             dividend = my_list_1[i]
             divisor = my_list_2[i]
-            # Check if either of the elements is not an integer or float
             if not isinstance(dividend, (int, float)) or not isinstance(divisor, (int, float)):
                 print("wrong type")
             else:
